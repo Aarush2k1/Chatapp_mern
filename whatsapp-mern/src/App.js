a4837c6a45f972a4f1d3cfaeefaed978cd72a28e
Aarush2k1 <aarukansal2k1@gmail.com>
2021-04-13 10:39:51 +0530

    c

--- whatsapp-mern/src/App.js
+++ whatsapp-mern/src/App.js
@@ -1,44 +1,44 @@
 import React, { useEffect, useState } from "react";
-import './App.css';
-import Sidebar from "./Sidebar";
-import Chat from "./Chat";
+import "./App.css";
+import Sidebar from "./components/Sidebar/Sidebar";
+import Chat from "./components/Chat/Chat";
 import Pusher from "pusher-js";
-import axios from './axios';
+import axios from "./axios";
 
 function App() {
-    const [messages, setMessages] = useState([]);
+  const [messages, setMessages] = useState([]);
 
-    useEffect(()=>{
-        axios.get('/messages/sync').then(response => {
-            setMessages(response.data);
-        });
-    },[]);
-    useEffect(() => {
-        const pusher = new Pusher('7d4a27394e6e28bf4555', {
-            cluster: 'ap2'
-        });
+  useEffect(() => {
+    axios.get("/messages/sync").then((response) => {
+      setMessages(response.data);
+    });
+  }, []);
+  useEffect(() => {
+    const pusher = new Pusher("7d4a27394e6e28bf4555", {
+      cluster: "ap2",
+    });
 
-        const channel = pusher.subscribe('messages');
-        channel.bind('inserted',(data)=> {
-            // alert(JSON.stringify(data));
-            setMessages([...messages,data])
-        });
+    const channel = pusher.subscribe("messages");
+    channel.bind("inserted", (data) => {
+      // alert(JSON.stringify(data));
+      setMessages([...messages, data]);
+    });
 
-        return() => {
-            channel.unbind_all();
-            channel.unsubscribe();
-        };
-    }, [messages])
-    console.log(messages);
+    return () => {
+      channel.unbind_all();
+      channel.unsubscribe();
+    };
+  }, [messages]);
+  console.log(messages);
 
-    return ( 
-        <div className = "app" >
-            <div className = "app_body" >
-                <Sidebar />
-                    <Chat messages={messages}/>
-            </div>
-        </div>
-    );
+  return (
+    <div className="app">
+      <div className="app_body">
+        <Sidebar />
+        <Chat messages={messages} />
+      </div>
+    </div>
+  );
 }
 
 export default App;
